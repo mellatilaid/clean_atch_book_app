1bc195c3fe6f43321e7a50ce2949879ccb4e213e
mellati <laidmellati@gmail.com>
2024-11-05 23:59:30 -0800

    created book entity

---
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -0,0 +1,12 @@
+class BookEntity {
+  final String title;
+  final String auther;
+  final num price;
+  final num rating;
+
+  BookEntity(
+      {required this.title,
+      required this.auther,
+      required this.price,
+      required this.rating});
+}
