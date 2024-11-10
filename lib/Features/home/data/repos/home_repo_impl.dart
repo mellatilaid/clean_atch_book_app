986b604b74d1dbed2e85ba8c91018b3ac033d94d
mellati <laidmellati@gmail.com>
2024-11-10 07:32:09 -0800

    create server failure from dio error

--- lib/Features/home/data/repos/home_repo_impl.dart
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -4,6 +4,7 @@ import 'package:bookly/Features/home/domain/entities/product_entity.dart';
 import 'package:bookly/Features/home/domain/repos/home_repo.dart';
 import 'package:bookly/core/errors/failures.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpl extends HomeRepo {
   final HomeRemoteDataSourse homeRemoteDataSourse;
@@ -21,8 +22,10 @@ class HomeRepoImpl extends HomeRepo {
       }
       final remoteProductList = await homeRemoteDataSourse.fetchProducts();
       return right(remoteProductList);
-    } on Exception catch (e) {
-      return left(Failures());
+    } on DioException catch (e) {
+      return left(ServerFailure.fromDioError(e));
+    } catch (e) {
+      return left(ServerFailure(errMessage: e.toString()));
     }
   }
 
--- lib/core/errors/failures.dart
+++ lib/core/errors/failures.dart
@@ -1,1 +1,49 @@
-class Failures {}
+import 'package:dio/dio.dart';
+
+class Failures {
+  final String errMessage;
+
+  const Failures({required this.errMessage});
+}
+
+class ServerFailure extends Failures {
+  const ServerFailure({required super.errMessage});
+
+  factory ServerFailure.fromDioError(DioException e) {
+    switch (e.type) {
+      case DioExceptionType.connectionError:
+        return const ServerFailure(
+            errMessage: 'Connection Error with ApiService');
+      case DioExceptionType.badResponse:
+        return ServerFailure.fromBadResponse(
+            e.response!.statusCode!, e.response!.data);
+      case DioExceptionType.cancel:
+        return const ServerFailure(errMessage: 'Request Canceled');
+      case DioExceptionType.connectionTimeout:
+        return const ServerFailure(
+            errMessage: 'Connection Timeout with ApiService');
+      case DioExceptionType.receiveTimeout:
+        return const ServerFailure(
+            errMessage: 'Receive Timeout with ApiService');
+      case DioExceptionType.sendTimeout:
+        return const ServerFailure(errMessage: 'Send Timeout with ApiService');
+      default:
+        return const ServerFailure(
+            errMessage: 'Opps there was an error, please try later');
+    }
+  }
+
+  factory ServerFailure.fromBadResponse(int statusCode, dynamic response) {
+    if (statusCode == 404) {
+      return const ServerFailure(errMessage: 'Your request not found');
+    } else if (statusCode >= 500) {
+      return const ServerFailure(
+          errMessage: 'Internal Server Error, Please try later');
+    } else if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
+      return ServerFailure(errMessage: response['error']);
+    } else {
+      return const ServerFailure(
+          errMessage: 'there was an unknows error, Please try again');
+    }
+  }
+}
